0dc030ed02cd9a4355653c0441074aab95ac03d0
Neo <pare.neojezer@gmail.com>
2025-12-14 00:16:55 +0800

    added createpost page and post functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,19 @@
-import axios from "axios";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
-import { useEffect } from "react";
-import { useState } from "react";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
-  const [listOfPosts, setListOfPosts] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:3001/posts").then((response) => {
-      setListOfPosts(response.data);
-      console.log(response.data);
-    });
-  }, []);
   return (
-    <div>
-      {listOfPosts.map((value, key) => {
-        return (
-          <div className="post">
-            <div className="title"> {value.title}</div>
-            <div className="body"> {value.postText}</div>
-            <div className="footer"> {value.username}</div>
-          </div>
-        );
-      })}
+    <div className="App">
+      <Router>
+        <Link to="/createpost"> Create A Post</Link>
+        <Link to="/"> Home Page</Link>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/createpost" exact element={<CreatePost />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
