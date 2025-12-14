ab3760106bae0ccc7775cb53a39562c4856dcf22
Shankssu <pare.neojezer@gmail.com>
2025-12-14 16:25:06 +0800

    added new routers for the comment section

---
+++ server/routes/Comments.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const { Comments } = require("../models");
+const { where } = require("sequelize");
+
+router.get("/:postId", async (req, res) => {
+  const postId = req.params.postId;
+  const post = await Comments.findAll({
+    where: { PostId: postId },
+  });
+  res.json(post);
+});
+
+router.post("/", async (req, res) => {
+  const comment = req.body;
+  await Comments.create(comment);
+  res.json(comment);
+});
+
+module.exports = router;
